c8528d03177bda89befa4266aee984277c986691
Mitchell <mwhyuen@gmail.com>
2022-03-03 17:01:57 +0200

    add seralizeError to customer Error class

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,10 +1,19 @@
 import { ValidationError } from "express-validator";
 
 export class DatabaseConnectionError extends Error {
+  statusCode = 500;
   reason = "Error connecting to database"
   constructor() {
     super();
 
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [
+      { 
+        message: this.reason
+      }
+    ]
   }
 }
