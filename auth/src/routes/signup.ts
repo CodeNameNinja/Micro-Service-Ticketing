3bde060fe6a6c2f1e4e3a7e360e67ceff27b4f91
Mitchell <mwhyuen@gmail.com>
2022-03-08 11:49:31 +0200

    add bad request errors

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-error";
+import { BadRequestError } from "../errors/bad-request-error";
 const router = express.Router();
 
 router.post(
@@ -16,13 +17,16 @@ router.post(
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
+      throw new RequestValidationError(errors.array());
     }
     const { email, password } = req.body;
-
-    // Save the user
-    throw new DatabaseConnectionError();
-    res.send("User registered");
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      throw new BadRequestError("User already exists");
+    }
+    const user = User.build({ email, password });
+    await user.save();
+    res.status(201).send(user);
   }
 );
 
