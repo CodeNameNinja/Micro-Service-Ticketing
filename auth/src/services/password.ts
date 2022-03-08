655d9d8879b26e56f8281b31fb999abf82eb7eb4
Mitchell <mwhyuen@gmail.com>
2022-03-08 13:51:51 +0200

    config hash password

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -1,6 +1,8 @@
 import { scrypt, randomBytes } from "crypto";
 import { promisify } from "util";
+
 const scryptAsync = promisify(scrypt);
+
 export class Password {
   static async toHash(password: string) {
     const salt = randomBytes(8).toString("hex");
@@ -10,7 +12,7 @@ export class Password {
   }
 
   static async compare(storedPassword: string, suppliedPassword: string) {
-    const [hashedPassword, salt] = suppliedPassword.split(".");
+    const [hashedPassword, salt] = storedPassword.split(".");
     const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
 
     return buf.toString("hex") === hashedPassword;
