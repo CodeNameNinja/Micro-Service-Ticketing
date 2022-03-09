8339b2968d7fff4a299396e595c485e6f806a3ef
Mitchell <mwhyuen@gmail.com>
2022-03-09 12:25:48 +0200

    add validationRequest middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,24 @@
-import express from "express";
-
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { validateRequest } from "../middelwares/validate-request";
 const router = express.Router();
 
-router.post('api/users/signin', (req, res) => {
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password").trim().notEmpty().withMessage("Password is required"),
+    validateRequest,
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+    const { email, password } = req.body;
     res.send("Hi there");
-})
+  }
+);
 
 export { router as signinRouter };
