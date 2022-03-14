e9f82c29ae1fcb0be8b8744a714ffe4eea6de0e3
Mitchell <mwhyuen@gmail.com>
2022-03-14 11:59:31 +0200

    feat: add unit tests for signup

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -22,13 +22,56 @@ it("returns a 400 with an invalid email", async () => {
     .expect(400);
 });
 
-//test invalid password 
+//test invalid password
 it("returns a 400 with an invalid password", async () => {
   return request(app)
     .post("/api/users/signup")
     .send({
       email: "test@test.com",
       password: "p",
+    })
+    .expect(400);
+});
+
+// test for email and password fields
+it("returns a 400 with missing email and password", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "",
+      password: "",
+    })
+    .expect(400);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+    })
+    .expect(400);
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      password: "password",
+    })
+    .expect(400);
+});
+
+// disallows duplicate emails
+it("disallows duplicate emails", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
     })
     .expect(400);
 });
