975571d7a1aa94edd61aac05135e5c9098de78dc
Mitchell <mwhyuen@gmail.com>
2022-03-15 13:54:53 +0200

    feat: add buildClient helper function

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,22 +1,10 @@
-import React from "react";
-import axios from "axios";
-
+import buildClient from "../api/build-client";
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
   return <div>Auth</div>;
 };
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === "undefined") {
-    const { data } = await axios.get(
-      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-      {
-        headers: req.headers,
-      }
-    );
-    return data;
-  } else {
-    const { data } = await axios.get("/api/users/currentuser");
-    return data;
-  }
+LandingPage.getInitialProps = async (context) => {
+  const { data } = await buildClient(context).get("/api/users/currentuser");
+  return data;
 };
 export default LandingPage;
