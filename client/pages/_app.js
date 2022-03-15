e74b9d25b1eda44404d7cd9e3af7f52809576ee5
Mitchell <mwhyuen@gmail.com>
2022-03-15 15:15:38 +0200

    add navbar

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,20 @@
 import "bootstrap/dist/css/bootstrap.css";
-
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+import buildClient from "../api/build-client";
+import Header from "../components/header";
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser}/>
+      <Component {...pageProps} />
+    </div>
+  );
 };
+AppComponent.getInitialProps = async ({ ctx, Component }) => {
+  const { data } = await buildClient(ctx).get("/api/users/currentuser");
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  return { pageProps, ...data };
+};
+export default AppComponent;
