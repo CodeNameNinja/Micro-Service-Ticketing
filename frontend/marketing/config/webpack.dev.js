4f34674703103f0f728d9696ed6d72c641389f72
Mitchell <mwhyuen@gmail.com>
2022-03-17 11:30:10 +0200

    update webpack config

--- frontend/marketing/config/webpack.dev.js
+++ frontend/marketing/config/webpack.dev.js
@@ -1,26 +1,29 @@
-const { merge } = require('webpack-merge');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common');
-const packageJson = require('../package.json');
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 
 const devConfig = {
-    mode: 'development',
-    devServer: {
-        port: 8081,
-        historyApiFallback: {
-            index: 'index.html'
-        }
+  mode: "development",
+  devServer: {
+    host: "0.0.0.0",
+    port: 8081,
+    historyApiFallback: {
+      index: "index.html",
     },
-    plugins: [
-        new ModuleFederationPlugin({
-            name: 'marketing',
-            filename: 'remoteEntry.js',
-            exposes: {
-                './core': './src/bootstrap'
-            },
-            shared: packageJson.dependencies,
-        })
-    ]
-}
+    compress: true,
+    disableHostCheck: true, // That solved it
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./core": "./src/bootstrap",
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
 
 module.exports = merge(commonConfig, devConfig);
