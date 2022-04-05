ba4e8418fb4766dfb52a933aa75ef84b6ca64615
Mitchell Yuen <mitchellyuen@Mitchells-Mac-mini.local>
2022-04-05 10:23:10 +0200

    feat: add ticket new route

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,16 +1,60 @@
 import express, { Request, Response } from "express";
-import { requireAuth, validateRequest } from "@channel360/common";
+import {
+  requireAuth,
+  validateRequest,
+  NotFoundError,
+  OrderStatus,
+  BadRequestError
+} from "@channel360/common";
 import { body } from "express-validator";
-
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 const router = express.Router();
-
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 router.post(
   "/api/orders",
   requireAuth,
   [body("ticketId").not().isEmpty().withMessage("TicketId must be provided")],
   validateRequest,
   async (req: Request, res: Response) => {
-    res.send({});
+    const { ticketId } = req.body;
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+    // Make sure that this ticket is not already reserved
+
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError("Ticket is already reserved");
+    }
+
+    // Calculate the expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+    // Build the order and save it to the database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+    await order.save();
+    // Publish an event saying that an order was created
+    // new OrderCreatedPublisher(natsWrapper.client).publish({
+    //   id: order.id,
+    //   version: order.version,
+    //   status: order.status,
+    //   userId: order.userId,
+    //   expiresAt: order.expiresAt.toISOString(),
+    //   ticket: {
+    //     id: ticket.id,
+    //     price: ticket.price,
+    //   },
+    // });
+
+    res.status(201).send(order);
   }
 );
 
