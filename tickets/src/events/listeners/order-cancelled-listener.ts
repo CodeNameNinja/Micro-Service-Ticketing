b60cbc7bab464f6b1976a56025dfac1a0bc6c536
Mitchell <mwhyuen@gmail.com>
2022-04-09 22:05:35 +0200

    feat: add order cancelled event listener

--- tickets/src/events/listeners/order-cancelled-listener.ts
+++ tickets/src/events/listeners/order-cancelled-listener.ts
@@ -1,24 +1,23 @@
-import { Listener, OrderCreatedEvent, Subjects } from "@channel360/common";
+import { Listener, OrderCancelledEvent, Subjects } from "@channel360/common";
 import { Message } from "node-nats-streaming";
 import { Ticket } from "../../models/ticket";
 import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
 import { queueGroupName } from "./queue-group-name";
 
-export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
-  readonly subject:Subjects.OrderCreated = Subjects.OrderCreated;
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  readonly subject:Subjects.OrderCancelled = Subjects.OrderCancelled;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent["data"], msg:Message) {
-    console.log("Event data!", data);
-    // find the ticket that the order is reserving. 
+  async onMessage(data: OrderCancelledEvent["data"], msg: Message) {
+    // Find the ticket that the order is reserving. 
     const ticket = await Ticket.findById(data.ticket.id);
-    // if no ticket, then throw error.
+    // If no ticket, then throw error.
     if (!ticket) {
       throw new Error("Ticket not found");
     }
-    // set the orderId property of the ticket
-    ticket.set({ orderId: data.id });
-    // save the ticket
+    // Set the orderId property of the ticket
+    ticket.set({ orderId: undefined });
+    // Save the ticket
     await ticket.save();
     await new TicketUpdatedPublisher(this.client).publish({
       id: ticket.id,
